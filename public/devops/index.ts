b8fe7a211142189670ddcb930572624e1e37cfd7
Aaryan Patel <a52patel@uwaterloo.ca>
2025-07-10 15:15:56 -0400

    initial redesign setup

--- public/devops/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Aws } from "./aws.svg";
-export { default as Docker } from "./docker.svg";
-export { default as Gcp } from "./gcp.svg";
-export { default as Git } from "./git.svg";
-export { default as Jenkins } from "./jenkins.svg";
-export { default as Kube } from "./kube.svg";
-export { default as Terraform } from "./terraform.svg";
-export { default as CircleCI } from "./circleci.svg";
--- public/languages/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Cpp } from "./cpp.svg";
-export { default as Cs } from "./cs.svg";
-export { default as Python } from "./python.svg";
-export { default as Sql } from "./sql.svg";
-export { default as Java } from "./java.svg";
-export { default as Ts } from "./ts.svg";
-export { default as Html } from "./html.svg";
-export { default as Css } from "./css.svg";
