38d8efc2be25a52437abc39ba1efe4764fd48991
Aaryan Patel <86172867+patel-aaryan@users.noreply.github.com>
2025-07-27 22:41:13 -0400

    icon cleanup

--- public/backend/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Djgnao } from "./django.svg";
-export { default as DotNet } from "./dotNet.svg";
-export { default as Fastify } from "./fastify.svg";
-export { default as Mongodb } from "./mongodb.svg";
-export { default as Mysql } from "./mysql.svg";
-export { default as Nest } from "./nest.svg";
-export { default as Node } from "./node.svg";
-export { default as Postgres } from "./postgres.svg";
--- public/frontend/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Angular } from "./angular.svg";
-export { default as Figma } from "./figma.svg";
-export { default as Jquery } from "./jquery.svg";
-export { default as Next } from "./next.svg";
-export { default as React } from "./react.svg";
-export { default as Redux } from "./redux.svg";
-export { default as Tailwind } from "./tailwind.svg";
-export { default as Vitest } from "./vitest.svg";
